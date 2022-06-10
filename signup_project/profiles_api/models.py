b12becb0bf76175bea0576620d3fd4a44dbb8696
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2022-06-10 05:20:21 +0200

    Fixes field name

--- signup_project/profiles_api/models.py
+++ signup_project/profiles_api/models.py
@@ -9,9 +9,9 @@ class Profile(models.Model):
     surname = models.CharField("last name", max_length=200)
     email = models.EmailField("e-mail address",max_length=200)
     phone = PhoneNumberField()
-    validated_email = models.BooleanField(default=False)
-    validatet_phone = models.BooleanField(default=False)
+    validated_email = models.BooleanField("validated e-mail", default=False)
+    validated_phone = models.BooleanField("validated phone number", default=False)
     def __str__(self):
         return "%s, %s" % (self.surname, self.name)
     def __repr__(self):
-        return "%s, %s - %s, %s - %s, %s" % (self.surname, self.name, self.email, self.validated_email, self.phone, self.validatet_phone)
+        return "%s, %s - %s, %s - %s, %s" % (self.surname, self.name, self.email, self.validated_email, self.phone, self.validated_phone)
