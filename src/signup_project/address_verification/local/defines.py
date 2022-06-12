00afb6ba6df89021108ededfd8c54f9ec6280eb4
Daniel Banore Dopico <daniel.banobre.dopico@pm.me>
2022-06-12 09:26:29 +0200

    Provides custom exceptions

--- src/signup_project/address_verification/local/defines.py
+++ src/signup_project/address_verification/local/defines.py
@@ -7,3 +7,11 @@ class VERIFIABLE_FIELDS(Enum):
 class TOKEN_CLAIMS(Enum):
     AUDIENCE = "address_verification",
     ISSUER = "address_verification",
+
+class MissingRequiredSecretException(Exception):
+    """Raise when no secret provided by env nor parameter on instantiation."""
+    pass
+
+class MissingRequiredPropertiesException(Exception):
+    """Raise when trying to get a JWT whitout provide all required data."""
+    pass
