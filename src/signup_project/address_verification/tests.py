8517ff9814bf2c054ed7d49265a3c659f9ae7396
Daniel Banore Dopico <daniel.banobre.dopico@pm.me>
2022-06-12 09:27:32 +0200

    First tests draft

--- src/signup_project/address_verification/tests.py
+++ src/signup_project/address_verification/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .local.VerificationToken import Profile
+from .local.VerificationToken import VerificationToken
+
+class VerificationTokenTestCase(TestCase):
+    def setUp(self) -> None:
+        Profile.objects.create(
+            name = "Daniel",
+            surname = "Bañobre Dopico",
+            email = "cosas@proton.me",
+            phone = "+34616656123",
+        )
+    def test_token_creation(self) -> None:
+        profile = Profile.objects.get(name="Daniel")
+        token = VerificationToken(secret = "Not a good secret")
+        token.profile = profile
+        token.verify_property = "phone"
+        token.property_spected_value = profile.phone
+        self.assertIs(type(token.jwt), str )
+    def test_parse_token(self) -> None:
+        pass
+    def test_parse_bad_token(self) -> None:
+        pass
+    def test_validate_data(self) -> None:
+        pass
+
+
