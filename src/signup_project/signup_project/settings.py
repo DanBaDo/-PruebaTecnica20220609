0b912af57d91858d3f68c16abb8519693f58c9ac
Daniel Banore Dopico <daniel.banobre.dopico@pm.me>
2022-06-12 09:43:57 +0200

    Claning django-phonenumber-field in settings

--- src/signup_project/signup_project/settings.py
+++ src/signup_project/signup_project/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # From django-phonenumber-field[phonenumberslite] pip package
-    'phonenumber_field',
     'profiles_api',
     'rest_framework',
 ]
