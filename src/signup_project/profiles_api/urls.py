00e45a02689741e69571b5adf726993ae4101888
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2022-06-12 18:22:10 +0200

    Validation token endpoint

--- src/signup_project/profiles_api/urls.py
+++ src/signup_project/profiles_api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import ProfilesView
+from .views import ProfilesView, ProfilesAddressesValidation
 
 urlpatterns = [
     path('profiles/', ProfilesView.as_view()),
     path('profiles/<int:pk>/', ProfilesView.as_view()),
+    path('profiles/validate/<str:jwt>', ProfilesAddressesValidation.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- src/signup_project/profiles_api/views.py
+++ src/signup_project/profiles_api/views.py
@@ -1,10 +1,11 @@
-from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_200_OK
 
 from profiles_api.models import Profile
 from profiles_api.serializers import PostProfileSerializer, GetProfileSerializer
+
+from local.VerificationToken import VerificationToken
 
 class ProfilesView(APIView):
 
@@ -21,3 +22,18 @@ class ProfilesView(APIView):
         serializer = GetProfileSerializer(profiles, many = True)
         return Response(serializer.data)
 
+class ProfilesAddressesValidation(APIView):
+
+    def get(self, request, jwt):
+        try:
+            verifier = VerificationToken()
+            validation = verifier.parse_token(jwt)
+            if validation["valid"]:
+                profile = Profile.objects.get(pk=validation["profile_id"])
+                setattr(profile, validation["change_flag"], True)
+                # TODO: Set as verified
+                return Response(f'Valid {validation["verify_property"]}', status=HTTP_200_OK)
+            else:
+                raise Exception
+        except Exception:
+            return Response("Invalid token", status=HTTP_400_BAD_REQUEST)
